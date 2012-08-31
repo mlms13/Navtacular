c900ed7a11e6b4e123d35a489071d7a51739d068
Michael Martin-Smucker <mlmartin13@gmail.com>
2012-08-31 15:39:57 -0400

    Use a more generic name for the class

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,18 +1,18 @@
 /* Calculate the right edge of the menu and align it to the right if needed */
 
 $(function () {
-    $(".dept-navbar").each(function () {
+    $('.navbar').each(function () {
         var $this = $(this),
             navbarRight = $this.offset().left + $this.outerWidth(); // The right edge of the navbar
 
-        $this.find("li > div.menu:not(.wide)").each(function () {
+        $this.find('li > div.menu:not(.wide)').each(function () {
             var $this = $(this),
                 menuRight = $this.offset().left + $this.outerWidth(); // The right edge of the menu
 
             // If the right edge of the menu extends past the right edge of the navbar...
             if(menuRight > navbarRight) {
                 // ...add a special class so that our css can align the menu to the right
-                $this.parent().addClass("menu-align-right")
+                $this.parent().addClass('menu-align-right');
             }
         })
     });
