a4d85a6fec366323f1f7251be3e55ed5e9702331
Michael Martin-Smucker <mlmartin13@gmail.com>
2013-11-01 14:51:22 -0400

    Add live reloading and more
    More grunt magic

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,6 @@
+
+var path = require('path');
+
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -31,12 +34,38 @@ module.exports = function (grunt) {
                     "css/navbar.min.css": "css/navbar.css"
                 }
             }
+        },
+        express: {
+            all: {
+                options: {
+                    bases: path.resolve('./'),
+                    livereload: true
+                }
+            }
+        },
+        watch: {
+            all: {
+                files: ['**/*.js', '**/*.less'],
+                tasks: ['less', 'autoprefixer'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        open: {
+            all: {
+                path: 'http://localhost:3000/demo.html'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-express');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-open');
 
     grunt.registerTask('default', ['less', 'autoprefixer', 'cssmin']);
+    grunt.registerTask('server', ['less', 'autoprefixer', 'cssmin', 'express', 'open', 'watch']);
 };
