801d64af3338f0c64b516f793700e1152ba9f6e4
Michael Martin-Smucker <mlmartin13@gmail.com>
2013-11-04 13:24:25 -0500

    Update menu-align-right script
    The script now works with the latest classes, and the demo has been
    updated to include a test case for the script.

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,19 +1,20 @@
-/* Calculate the right edge of the menu and align it to the right if needed */
 
 $(function () {
     $('.navbar').each(function () {
-        var $this = $(this),
-            navbarRight = $this.offset().left + $this.outerWidth(); // The right edge of the navbar
+        var $navbar = $(this),
+            navbarRight = $navbar.offset().left + $navbar.outerWidth(); // right edge of the navbar
 
-        $this.find('li > div.menu:not(.wide)').each(function () {
-            var $this = $(this),
-                menuRight = $this.offset().left + $this.outerWidth(); // The right edge of the menu
+        // For each non-mega menu,
+        // fix the alignment if it extends beyond the right edge of the navbar
+        $navbar.find('.navbar-menu:not(.mega)').each(function () {
+            var $menu = $(this),
+                menuRight = $menu.offset().left + $menu.outerWidth(); // right edge of the menu
 
             // If the right edge of the menu extends past the right edge of the navbar...
-            if(menuRight > navbarRight) {
+            if (menuRight > navbarRight) {
                 // ...add a special class so that our css can align the menu to the right
-                $this.parent().addClass('menu-align-right');
+                $menu.parent().addClass('menu-align-right');
             }
-        })
+        });
     });
 });
