81b0af7e54d589ed0ad5591080ebe4cfd270927c
stezu <stephen@macbookair.home>
2013-11-12 20:53:53 -0500

    Fixed click handlers
    Removed the multiple click handlers on the sub-menus and added
    !important to the display on sub-menu on large screens. It might need
    some tweaking since I only put it in one place.

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -14,32 +14,28 @@ function handleMobileNav() {
         $navbar.prepend('<h1 class="navbar-label">Navigation</h1>');
     }
     // find all items that have a child menu
-    if (small) {
-        $navbar.find('.navbar-menu').each(function () {
-            var $menu = $(this),
-                $link = $menu.prev('.navbar-link');
+    $navbar.find('.navbar-menu').each(function () {
+        var $menu = $(this),
+            $link = $menu.prev('.navbar-link');
 
-            // add a dropdown icon
-            if ($link.has('.icon-caret-down').length === 0) {
-                $link.append('<i class="icon-caret-down"></i>');
+        // add a dropdown icon
+        if ($link.has('.icon-caret-down').length === 0) {
+            $link.append('<i class="icon-caret-down"></i>');
+        }
+        // and make the link toggle the menu
+        $link.unbind().on('click', function () {
+            if ($menu.is(':visible')) {
+                $menu.slideUp();
+            } else {
+                $navbar.find('.navbar-menu').slideUp();
+                $menu.slideDown();
             }
-            // and make the link toggle the menu
-            $link.on('click', function () {
-                console.log('click'); // This is getting fired a bunch when the screen goes from big to small.
-                console.log(small);
-                if ($menu.is(':visible')) {
-                    $menu.slideUp();
-                } else {
-                    $navbar.find('.navbar-menu').slideUp();
-                    $menu.slideDown();
-                }
-            });
         });
-    }
+    });
 }
 
 // handle toggling the menu
-$navbar.find('.navbar-label').on('click touchstart', function (e) {
+$navbar.find('.navbar-label').on('click touchstart', function () {
     $('html').toggleClass('nav-visible');
     return false;
 });
