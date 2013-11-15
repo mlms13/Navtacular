cb3225e34fd1ad7ba69577b18ed20630a83f22c3
szuniga13 <szuniga@health.usf.edu>
2013-11-15 08:35:17 -0500

    Made navbar.js more terse

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,52 +1,35 @@
 var $navbar = $('.navbar');
 
 function handleMobileNav() {
-    if ( $(window).width() < 630 ) {
-        $navbar.addClass('smallScreen');
-    } else {
-        $navbar.removeClass('smallScreen');
+    if ( $(window).width() > 630 ) {
+        return;
     }
-
-    var small = $navbar.hasClass('smallScreen');
 
     // make sure .navbar-label exists
-    if (small && $navbar.has('.navbar-label').length === 0) {
+    if ($navbar.has('.navbar-label').length === 0) {
         $navbar.prepend('<h1 class="navbar-label">Navigation</h1>');
     }
+
     // find all items that have a child menu
-    if (small) {
-        $navbar.find('.navbar-menu').each(function () {
-            var $menu = $(this),
-                $link = $menu.prev('.navbar-link');
+    $navbar.find('.navbar-menu').each(function () {
+        var $menu = $(this),
+            $link = $menu.prev('.navbar-link');
 
-            // add a dropdown icon
-            if ($link.has('.icon-caret-down').length === 0) {
-                $link.append('<i class="icon-caret-down"></i>');
+        // add a dropdown icon
+        if ($link.has('.icon-caret-down').length === 0) {
+            $link.append('<i class="icon-caret-down"></i>');
+        }
+        // and make the link toggle the menu
+        $link.unbind().on('click', function () {
+            if ($menu.is(':visible')) {
+                $menu.slideUp();
+            } else {
+                $navbar.find('.navbar-menu').slideUp();
+                $menu.slideDown();
             }
-            // and make the link toggle the menu
-            $link.unbind().on('click', function () {
-                if ($menu.is(':visible')) {
-                    $menu.slideUp();
-                } else {
-                    $navbar.find('.navbar-menu').slideUp();
-                    $menu.slideDown();
-                }
-            });
         });
-    }
+    });
 }
-
-// handle toggling the menu
-$navbar.find('.navbar-label').on('click touchstart', function () {
-    $('html').toggleClass('nav-visible');
-    return false;
-});
-$navbar.on('click touchstart', function (e) {
-    e.stopPropagation();
-});
-$(document).on('click touchstart', function () {
-    $('html').removeClass('nav-visible');
-});
 
 $(function () {
     $('.navbar').each(function () {
@@ -66,6 +49,19 @@ $(function () {
             }
         });
     });
+
+    // handle toggling the menu
+    $navbar.find('.navbar-label').on('click touchstart', function () {
+        $('html').toggleClass('nav-visible');
+        return false;
+    });
+    $navbar.on('click touchstart', function (e) {
+        e.stopPropagation();
+    });
+    $(document).on('click touchstart', function () {
+        $('html').removeClass('nav-visible');
+    });
+
     handleMobileNav();
 });
 
