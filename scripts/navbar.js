4763cf8df536543ca61665529938b0657a488757
Michael Martin-Smucker <mlmartin13@gmail.com>
2013-11-16 18:37:24 -0500

    Convert all of the JS to a jQuery plugin
    This gets rid of all the globals and gives us all the flexibility of a
    jQuery plugin.  It's probably overkill since we're trying to use minimal
    Javascript, but at least it helps out with code organization.

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,76 +1,79 @@
-var $navbar = $('.navbar');
+(function ($) {
+    $.fn.navtacular = function () {
+        var $navbar = this;
 
-function handleMobileNav() {
-    if ( $(window).width() > 630 ) {
-        return;
-    }
-
-    // find all items that have a child menu
-    $navbar.find('.navbar-menu').each(function () {
-        var $menu = $(this),
-            $link = $menu.prev('.navbar-link');
-
-        // and make the link toggle the menu
-        $link.unbind().on('click', function () {
-            if ($menu.is(':visible')) {
-                $menu.slideUp();
-            } else {
-                $navbar.find('.navbar-menu').slideUp();
-                $menu.slideDown();
+        function handleMobileNav() {
+            if ( $(window).width() > 630 ) {
+                return;
             }
-        });
-    });
-}
 
-$(function () {
-    $('.navbar').each(function () {
-        var $navbar = $(this),
-            $menus = $navbar.find('.navbar-menu'),
-            navbarRight = $navbar.offset().left + $navbar.outerWidth(); // right edge of the navbar
+            // find all items that have a child menu
+            $navbar.find('.navbar-menu').each(function () {
+                var $menu = $(this),
+                    $link = $menu.prev('.navbar-link');
 
-        // make sure .navbar-label exists
-        if ($navbar.has('.navbar-label').length === 0) {
-            $navbar.prepend('<h1 class="navbar-label">Navigation</h1>');
+                // and make the link toggle the menu
+                $link.unbind().on('click', function () {
+                    if ($menu.is(':visible')) {
+                        $menu.slideUp();
+                    } else {
+                        $navbar.find('.navbar-menu').slideUp();
+                        $menu.slideDown();
+                    }
+                });
+            });
         }
 
-        // add a dropdown icon (will only be displayed on mobile devices)
-        $menus.each(function () {
-            var $link = $(this).prev('.navbar-link');
-
-            if ($link.has('.icon-caret-down').length === 0) {
-                $link.append('<i class="icon-caret-down"></i>');
-            }
+        $(window).on('resize', function () {
+            handleMobileNav();
         });
 
-        // For each non-mega menu,
-        // fix the alignment if it extends beyond the right edge of the navbar
-        $menus.not('.mega').each(function () {
-            var $menu = $(this),
-                menuRight = $menu.offset().left + $menu.outerWidth(); // right edge of the menu
-
-            // If the right edge of the menu extends past the right edge of the navbar...
-            if (menuRight > navbarRight) {
-                // ...add a special class so that our css can align the menu to the right
-                $menu.parent().addClass('menu-align-right');
-            }
+        // handle toggling the menu
+        $navbar.find('.navbar-label').on('click touchstart', function () {
+            $('html').toggleClass('nav-visible');
+            return false;
         });
-    });
+        $navbar.on('click touchstart', function (e) {
+            e.stopPropagation();
+        });
+        $(document).on('click touchstart', function () {
+            $('html').removeClass('nav-visible');
+        });
+        handleMobileNav();
 
-    // handle toggling the menu
-    $navbar.find('.navbar-label').on('click touchstart', function () {
-        $('html').toggleClass('nav-visible');
-        return false;
-    });
-    $navbar.on('click touchstart', function (e) {
-        e.stopPropagation();
-    });
-    $(document).on('click touchstart', function () {
-        $('html').removeClass('nav-visible');
-    });
+        return this.each(function () {
+            var $navbar = $(this), // the current navbar while looping through each
+                $menus = $navbar.find('.navbar-menu'),
+                navbarRight = $navbar.offset().left + $navbar.outerWidth(); // right edge of the navbar
 
-    handleMobileNav();
-});
+            // make sure .navbar-label exists
+            if ($navbar.has('.navbar-label').length === 0) {
+                $navbar.prepend('<h1 class="navbar-label">Navigation</h1>');
+            }
 
-$(window).on('resize', function () {
-    handleMobileNav();
-});
+            // add a dropdown icon (will only be displayed on mobile devices)
+            $menus.each(function () {
+                var $link = $(this).prev('.navbar-link');
+
+                if ($link.has('.icon-caret-down').length === 0) {
+                    $link.append('<i class="icon-caret-down"></i>');
+                }
+            });
+
+            // For each non-mega menu,
+            // fix the alignment if it extends beyond the right edge of the navbar
+            $menus.not('.mega').each(function () {
+                var $menu = $(this),
+                    menuRight = $menu.offset().left + $menu.outerWidth(); // right edge of the menu
+
+                // If the right edge of the menu extends past the right edge of the navbar...
+                if (menuRight > navbarRight) {
+                    // ...add a special class so that our css can align the menu to the right
+                    $menu.parent().addClass('menu-align-right');
+                }
+            });
+        });
+    };
+
+    $('.navbar').navtacular();
+}(jQuery));
